5cd9357b126be11ef9ad7c436af7cfb14ead69cd
Casper <CasualX@users.noreply.github.com>
2017-08-11 22:00:33 +0200

    polygons 'n all that

--- src/d2/polygon.rs
+++ src/d2/polygon.rs
@@ -48,6 +48,7 @@ pub trait Polygon {
 	/// assert_eq!(pts.bounds(), result);
 	/// ```
 	fn bounds(&self) -> Rect;
+	fn ball(&self) -> (Point2, f32);
 	/// Calculates the signed area of the polygon.
 	///
 	/// If the polygon's signed area is positive its winding is said to be counter-clockwise and clockwise if its signed area is negative.
@@ -67,6 +68,43 @@ pub trait Polygon {
 	/// A zero result indicates the point lies outside the polygon.
 	fn winding_number(&self, pt: Point2) -> i32;
 	/// Calculates the enclosing convex hull of the polygon.
+	///
+	/// Returns indices into the polygon which represent the convex hull.
+	///
+	/// # Examples
+	///
+	/// <svg width="400" height="200" viewBox="-2 -3 8 8" xmlns="http://www.w3.org/2000/svg">
+	///   <polygon fill="none" stroke="green" stroke-width="0.05" points="-1,-1 1,-2 5,-1 5,2 4,4 2,3 -1,1" />
+	///   <circle cx="-1.0" cy="-1.0" r="0.1" />
+	///   <circle cx="-1.0" cy="1.0" r="0.1" />
+	///   <circle cx="1.0" cy="2.0" r="0.1" />
+	///   <circle cx="2.0" cy="3.0" r="0.1" />
+	///   <circle cx="3.0" cy="2.0" r="0.1" />
+	///   <circle cx="4.0" cy="4.0" r="0.1" />
+	///   <circle cx="5.0" cy="2.0" r="0.1" />
+	///   <circle cx="5.0" cy="-1.0" r="0.1" />
+	///   <circle cx="3.0" cy="-1.0" r="0.1" />
+	///   <circle cx="1.0" cy="-2.0" r="0.1" />
+	/// </svg>
+	///
+	/// The example below visualizes the input points and its convex hull (the green polygon).
+	///
+	/// ```
+	/// let pts = &[
+	/// 	Point2(-1.0, -1.0),
+	/// 	Point2(-1.0, 1.0),
+	/// 	Point2(1.0, 2.0),
+	/// 	Point2(2.0, 3.0),
+	/// 	Point2(3.0, 2.0),
+	/// 	Point2(4.0, 4.0),
+	/// 	Point2(5.0, 2.0),
+	/// 	Point2(5.0, -1.0),
+	/// 	Point2(3.0, -1.0),
+	/// 	Point2(1.0, -2.0),
+	/// ];
+	/// let hull = pts.convex_hull();
+	/// assert_eq!(hull, &[0, 10, 8, 6, 5, 3, 1]);
+	/// ```
 	fn convex_hull(&self) -> Vec<Index>;
 	/// Triangulates the polygon.
 	fn triangulate(&self) -> Vec<(Index, Index, Index)>;
@@ -93,6 +131,64 @@ impl<T: ?Sized + AsRef<[Point2]>> Polygon for T {
 			pts.iter().fold((pts[0], pts[0]), |acc, p| (acc.0.min(*p), acc.1.max(*p)))
 		};
 		Rect::new(mins, maxs)
+	}
+	fn ball(&self) -> (Point2, f32) {
+		let pts = self.as_ref();
+		if pts.is_empty() {
+			return (Point2::default(), 0.0);
+		}
+
+		let mut mins = pts[0];
+		let mut maxs = pts[0];
+		let (mut p_xmin, mut p_xmax, mut p_ymin, mut p_ymax) = (&pts[0], &pts[0], &pts[0], &pts[0]);
+		for p in &pts[1..] {
+			if p.x < mins.x {
+				mins.x = p.x;
+				p_xmin = p;
+			}
+			else if p.x > maxs.x {
+				maxs.x = p.x;
+				p_xmax = p;
+			}
+			if p.y < mins.y {
+				mins.y = p.y;
+				p_ymin = p;
+			}
+			else if p.y > maxs.y {
+				maxs.y = p.y;
+				p_ymax = p;
+			}
+		}
+
+		let mut p_c;
+		let mut radius2;
+		let mut radius;
+
+		let p_dx = *p_xmax - *p_xmin;
+		let p_dy = *p_ymax - *p_ymin;
+		if p_dx.len_sqr() > p_dy.len_sqr() {
+			p_c = *p_xmin + p_dx * 0.5;
+			radius2 = (*p_xmax - p_c).len_sqr();
+		}
+		else {
+			p_c = *p_ymin + p_dy * 0.5;
+			radius2 = (*p_ymax - p_c).len_sqr();
+		}
+		radius = radius2.sqrt();
+
+		for &p in pts {
+			let d_p = p - p_c;
+			let dist2 = d_p.len_sqr();
+			if dist2 <= radius2 {
+				continue;
+			}
+			let dist = dist2.sqrt();
+			radius = (radius + dist) * 0.5;
+			radius2 = radius * radius;
+			p_c = p_c + d_p * ((dist - radius) / dist);
+		}
+
+		(p_c, radius)
 	}
 	fn signed_area(&self) -> f32 {
 		let pts = self.as_ref();
@@ -188,14 +284,14 @@ impl<T: ?Sized + AsRef<[Point2]>> Polygon for T {
 			}
 			&mut array[..pts.len()]
 		};
+		// TODO: Unstable sort when available
 		sorted.sort_by(|&a, &b| {
 			use ::std::cmp::Ordering;
 			let a = pts[a as usize];
 			let b = pts[b as usize];
-			let d = if a.x == b.x { a.y - b.y } else { a.x - b.x };
-			if d < 0.0 { Ordering::Less }
-			else if d > 0.0 { Ordering::Greater }
-			else { Ordering::Equal }
+			let order = if a.x == b.x { a.y < b.y } else { a.x < b.x };
+			if order { Ordering::Less }
+			else { Ordering::Greater }
 		});
 
 		// Allocate worst-case space
